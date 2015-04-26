410a2ad0f13bd4bef02b1d7c53b30e5966a05597
Vitaliy Slobodin <vitaliy.slobodin@gmail.com>
2015-04-26 17:14:06 +0300

    Redirect to HTTPS

--- index.js
+++ index.js
@@ -8,11 +8,22 @@ https.createServer({
   cert: fs.readFileSync('cert.pem'),
   passphrase: 'denis'
 }, app).listen(443, function() {
-  console.log('Listening on localhost:3000');
+  console.log('Listening on localhost:80');
+});
+
+app.listen(80, function() {
+  console.log('Listening on 80');
 });
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
+
+app.use(function requireHTTPS(req, res, next) {
+  if (!req.secure) {
+    return res.redirect('https://' + req.headers.host + req.url);
+  }
+  next();
+});
 
 app.get('/', function (req, res) {
   return res.sendfile('index.html');
