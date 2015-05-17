8db2e7e9910759199d8a4af9c64a50c54570bd85
denismosolov <denismosolov@gmail.com>
2015-05-17 10:22:01 +0300

    ssl added, relate #1

--- Vagrantfile
+++ Vagrantfile
@@ -12,6 +12,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provision :shell, path: "vagrant.sh", privileged: false
 
   config.vm.network :forwarded_port, host: 4567, guest: 80
+  config.vm.network :forwarded_port, host: 4568, guest: 443
 
   config.vm.network "private_network", ip: "192.168.50.4"
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,21 @@ apt-get install -y nginx
 #mkdir -p /var/www/russian-lesson.com/public_html
 #chown -R www-data:www-data /var/www/russian-lesson.com/public_html
 #chmod 755 /var/www
-cp /vagrant/vagrant_provision/russian-lesson.com /etc/nginx/sites-available/russian-lesson.com
+cp /vagrant/vagrant_provision/russian_lesson_com /etc/nginx/sites-available/russian_lesson_com
 cp /vagrant/vagrant_provision/hosts /etc/hosts
-cp /vagrant/vagrant_provision/sendfile.conf /etc/nginx/conf.d/sendfile.conf
-if [ ! -f /etc/nginx/sites-enabled/russian-lesson.com ]; then
-	ln -s /etc/nginx/sites-available/russian-lesson.com /etc/nginx/sites-enabled/russian-lesson.com
+sed -i 's/sendfile on/sendfile off/' /etc/nginx/nginx.conf
+if [ ! -f /etc/nginx/sites-enabled/russian_lesson_com ]; then
+	ln -s /etc/nginx/sites-available/russian_lesson_com /etc/nginx/sites-enabled/russian_lesson_com
 fi
 if [ -f /etc/nginx/sites-enabled/default ]; then
 	rm /etc/nginx/sites-enabled/default
+fi
+mkdir -p /etc/nginx/ssl
+if [ ! -f /etc/nginx/ssl/russian_lesson_com.key ]; then
+	cp /vagrant/vagrant_provision/russian_lesson_com.key /etc/nginx/ssl/russian_lesson_com.key
+fi
+if [ ! -f /etc/nginx/ssl/russian_lesson_com.crt ]; then
+	cp /vagrant/vagrant_provision/russian_lesson_com.crt /etc/nginx/ssl/russian_lesson_com.crt
 fi
 service nginx restart
 
